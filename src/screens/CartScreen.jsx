a2e13a6752948b490033c0deb1247c426a9a5546
MUHD-UNAIS <unaispoonthala8086@gmail.com>
2026-06-25 16:29:22 +0530

    connected cart to supabase

--- src/screens/CartScreen.jsx
+++ src/screens/CartScreen.jsx
@@ -1,12 +1,51 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import { Trash2, Plus, Minus, ShoppingBag } from 'lucide-react';
 import { Link } from 'react-router-dom';
 import { useCart } from '../context/CartContext';
 import BackButton from '../components/BackButton';
+import { supabase } from '../supabase';
 import '../styles/CartScreen.css';
 
 const CartScreen = () => {
-  const { cartItems, updateQuantity, removeFromCart, cartTotal } = useCart();
+  const {
+    cartItems,
+    setCartItems,
+    updateQuantity,
+    removeFromCart,
+    cartTotal
+  } = useCart();
+
+  const fetchCart = async () => {
+      const {
+        data: { user }
+      } = await supabase.auth.getUser();
+
+      if (!user) return;
+
+      const { data, error } = await supabase
+        .schema('marketplace_dataspace')
+        .from('cart_items')
+        .select(`
+          quantity,
+          products (*)
+        `)
+        .eq('buyer_id', user.id);
+
+      if (error) {
+        console.error(error);
+        return;
+      }
+
+      const formattedCart = data.map(item => ({
+        ...item.products,
+        quantity: item.quantity
+      }));
+
+      setCartItems(formattedCart);
+  };
+  useEffect(() => {
+    fetchCart();
+  }, []);
 
   if (cartItems.length === 0) {
     return (
@@ -21,7 +60,7 @@ const CartScreen = () => {
       </div>
     );
   }
-  console.log(cartItems[0]);
+
   return (
     <div className="section cart-page" style={{ paddingTop: '120px' }}>
       <div className="container">
